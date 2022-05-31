427b7a4f55e046cb77efaa660136d46af64b6b2d
Devlin Duldulao <webmasterdevlin@gmail.com>
2022-05-31 23:58:43 +0200

    update

--- TodoApi/app/Http/Controllers/TodoController.php
+++ TodoApi/app/Http/Controllers/TodoController.php
@@ -9,7 +9,11 @@ class TodoController extends Controller
     public function show($todo_id): JsonResponse
     {
         $random_number = rand(0, 100);
-        $data = ["id" => $todo_id, "activity" => $random_number];
+        if ($random_number >= $todo_id) {
+            error_log("--> TodoService Returned 500 ERROR");
+            return response()->json(['message' => 'Todo Service is down'], 500);
+        }
+        $data = ["id" => $todo_id, "activity" => "eat"];
         return response()->json($data, 200);
     }
 }
