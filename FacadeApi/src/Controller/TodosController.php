2a0079334b8cbafaec823c3ed087f7cf616341cb
Devlin Duldulao <webmasterdevlin@gmail.com>
2022-06-01 13:37:06 +0200

    update

--- FacadeApi/src/Controller/TodosController.php
+++ FacadeApi/src/Controller/TodosController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class TodosController extends AbstractController
 {
-    #[Route('/todos', name: 'todos')]
-    public function index(): JsonResponse
+    private string $baseUrl = 'http://localhost:8001/api/';
+
+    #[Route('/todos/{id}', name: 'todo_by_id', methods: ['GET'])]
+    public function show($id): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TodosController.php',
-        ], 200);
+        try {
+            $client = new Client(['base_uri' => $this->baseUrl]);
+            $response = $client->request('GET', 'todos/' . $id);
+            $data = json_decode($response->getBody()->getContents(), true);
+            return new JsonResponse($data);
+        } catch (GuzzleException $e) {
+            return new JsonResponse([
+                'error' => 'Todo ' . $id .' not found'
+            ], 500);
+        }
     }
 }
