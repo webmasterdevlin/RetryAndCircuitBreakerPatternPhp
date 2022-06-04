6f9644652d2e1f0773abf8656d662b7c48b292cc
Devlin Duldulao <webmasterdevlin@gmail.com>
2022-06-04 23:11:30 +0200

    retry policy only

--- FacadeApi/app/Http/Controllers/TodoController.php
+++ FacadeApi/app/Http/Controllers/TodoController.php
@@ -14,24 +14,14 @@ class TodoController extends Controller
 
     public function show($todo_id): JsonResponse
     {
-        $limiter = app(RateLimiter::class);
-        $action_key = 'todo_service';
-        $threshold = 2;
-        if ($limiter->tooManyAttempts($action_key, $threshold)) {
-            return new JsonResponse([
-                "message" => "Exceeded the maximum number of failed attempts. Please wait for 2 minutes"],
-                500);
-        } else {
-            try {
-                $response = Http::retry(5, 5000)->get($this->baseUrl . 'todos/' . $todo_id);
-                error_log("--> FacadeApi RECEIVED a SUCCESS");
-                $data = $response->json();
-                return new JsonResponse($data, 200);
-            } catch (Exception $e) {
-                error_log("--> FacadeApi RECEIVED a FAILURE");
-                $limiter->hit($action_key, Carbon::now()->addMinutes(2));
-                return new JsonResponse(["message" => "The server is not responding after 5 attempts."], 500);
-            }
+        try {
+            $response = Http::retry(3, 5000)->get($this->baseUrl . 'todos/' . $todo_id);
+            error_log("--> FacadeApi RECEIVED a SUCCESS");
+            $data = $response->json();
+            return new JsonResponse($data, 200);
+        } catch (Exception $e) {
+            error_log("--> FacadeApi RECEIVED a FAILURE");
+            return new JsonResponse(["message" => "The server is not responding after 3 attempts."], 500);
         }
     }
 }
